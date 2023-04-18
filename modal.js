dd736068ac73b6d0e6375a31045f485ad7863dc8
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2023-04-18 11:18:52 +0200

    galerija na single project stranici

--- modal.js
+++ modal.js
@@ -17,3 +17,9 @@ document.addEventListener("click", function (e) {
     captionText.innerHTML = img.alt;
   }
 });
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
